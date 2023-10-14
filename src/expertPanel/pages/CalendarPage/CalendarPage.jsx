14dbe35aa62d4b7e67df4e64e2454e63e5ab65c2
Afraz33 <afraz3301@gmail.com>
2023-10-15 00:44:30 +0500

    Integrate Calendar with Backend

--- src/expertPanel/pages/CalendarPage/CalendarPage.jsx
+++ src/expertPanel/pages/CalendarPage/CalendarPage.jsx
@@ -1,7 +1,30 @@
 import Navbar from "../../layout/Navbar/Navbar";
 import Footer from "../../layout/Footer/Footer";
 import Calendar from "../../components/Calendar/Calendar";
+import { useState } from "react";
+import axios from "axios";
 function CalendarPage() {
+  const [startTime, setStartTime] = useState("");
+  const [endTime, setEndTime] = useState("");
+
+  const updateTimeSlots = async () => {
+    const timeSlotData = {
+      startTime: startTime,
+      endTime: endTime,
+    };
+    try {
+      const response = await axios.put(
+        "http://localhost:5000/api/experts/expert-updateCalendar",
+        {
+          email: localStorage.getItem("email"),
+          timeSlots: timeSlotData,
+        }
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.error("Error updating time slots", error);
+    }
+  };
   return (
     <div className="py-4 bg-[#EAE1E1] h-screen">
       <Navbar />
@@ -13,10 +36,18 @@ function CalendarPage() {
       </div>
       <div className=" mt-6 flex flex-col gap-8 ">
         <div className="mx-auto w-fit">
-          <Calendar />
+          <Calendar
+            startTime={startTime}
+            endTime={endTime}
+            setEndTime={setEndTime}
+            setStartTime={setStartTime}
+          />
         </div>
 
-        <button className="mx-auto bg-black text-white rounded-full px-4 text-lg py-2 w-fit hover:bg-[#B2A1FE] hover:text-black ">
+        <button
+          onClick={updateTimeSlots}
+          className="mx-auto bg-black text-white rounded-full px-4 text-lg py-2 w-fit hover:bg-[#B2A1FE] hover:text-black "
+        >
           Add Time-Slot
         </button>
       </div>
