f99c1386b5bb5cfafd298ed8ed211aee60b53418
Afraz33 <afraz3301@gmail.com>
2024-02-08 13:08:58 +0500

    fix career chatbot conversation

--- src/CareerchatbotPanel/components/conversationFlow/ConversationFlow.jsx
+++ src/CareerchatbotPanel/components/conversationFlow/ConversationFlow.jsx
@@ -329,7 +329,7 @@ const initialQuestions = [
   },
 ];
 function ConversationFlow({ setPersonality, setViewCareers, Personality }) {
-  const [index, setIndex] = useState(46);
+  const [index, setIndex] = useState(0);
   const [responding, setResponding] = useState(false);
   const [processing, setProcessing] = useState(false);
   const [processingMessage, setProcessingMessage] = useState("");
