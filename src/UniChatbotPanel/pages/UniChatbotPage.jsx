4382fb23cee9f0dd0ea188f262195f09bcceb96c
Muhammad-Saad12 <muhammadsaad7250@gmail.com>
2024-02-19 15:29:19 +0500

    comitting before pulling uni ranking

--- src/UniChatbotPanel/pages/UniChatbotPage.jsx
+++ src/UniChatbotPanel/pages/UniChatbotPage.jsx
@@ -2,16 +2,16 @@ import { useState } from "react";
 import ConversationFlow from "../components/conversationFlow/ConversationFlow";
 import SampleQuery from "../components/sampleQuery/SampleQuery";
 const questions = [
-  "What degree programs does FAST National University offer?",
-  "What is the semester fee of FAST National University?",
+  "What degree programs does fast national university offer?",
+  "What is the semester fee of fast national university?",
   "Name the departments of Rawalpindi Medical University.",
   "What entrance test does National University of Medical Sciences take?",
   "What's the semester fee of Lahore University of Management Sciences?",
   "I have an interest in architecture. Recommend me some universities that offer BS architecture.",
-  "Is financial aid available in FAST National University?",
+  "Is financial aid available in fast national university?",
   "I live in Rawalpindi so I want to join a university which is in Rawalpindi. So tell me those universities which are in Rawalpindi.",
-  "I got admission in BS Computer Science in both FAST National University and in National University of Science and Technology. Which university should I choose?",
-  "I got admission in BS Computer Science in both FAST National University and in National University of Science and Technology. Which university should I choose? I want a university in which financial aid is available and transportation is also available.",
+  "I got admission in BS Computer Science in both fast national university and in National University of Science and Technology. Which university should I choose?",
+  "I got admission in BS Computer Science in both fast national university and in National University of Science and Technology. Which university should I choose? I want a university in which financial aid is available and transportation is also available.",
   "Which university has a lower semester fee, Aga Khan University or Rawalpindi Medical University?",
   "I have an interest in zoology so recommend me those universities that have the department of zoology.",
 ];
